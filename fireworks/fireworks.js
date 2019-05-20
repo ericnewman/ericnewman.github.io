27ea85d5382146271dd3a840d0a5e9964206d77a
ericnewman@mac.com <ericnewman@mac.com>
2019-05-20 17:02:03 -0400

    Remove broken UI element

--- fireworks/fireworks.js
+++ fireworks/fireworks.js
@@ -624,32 +624,13 @@ var guiPresets = {
 
 
 var fworks = new Fireworks();
-var gui = new dat.GUI({
-	autoPlace: false,
-	load: guiPresets,
-	//preset: 'Default'
-	preset: 'Eric'
-});
+// var gui = new dat.GUI({
+// 	autoPlace: false,
+// 	load: guiPresets,
+// 	//preset: 'Default'
+// 	preset: 'Eric'
+// });
 
-/ This is the service worker with the Advanced caching
-
-// Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
-
-// Check compatibility for the browser we're running this in
-if ("serviceWorker" in navigator) {
-  if (navigator.serviceWorker.controller) {
-    console.log("[EJN] active service worker found, no need to register");
-  } else {
-    // Register the service worker
-    navigator.serviceWorker
-      .register("serviceworker-sw.js", {
-        scope: "./"
-      })
-      .then(function (reg) {
-        console.log("[EJN] Service worker has been registered for scope: " + reg.scope);
-      });
-  }
-}
 // var customContainer = document.getElementById('gui');
 // customContainer.appendChild(gui.domElement);
 
