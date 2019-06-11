3cc78fd5a774abce2d815a42c540da924908e488
ericnewman@mac.com <ericnewman@mac.com>
2019-06-11 10:40:59 -0400

    Fix relative paths

---
+++ games_poc/preact.config.js
@@ -0,0 +1,10 @@
+export default (config, env, helpers) => {
+     config.output.publicPath = '/games_poc/build/';
+
+    // use the public path in your app as 'process.env.PUBLIC_PATH'
+    config.plugins.push(
+      new helpers.webpack.DefinePlugin({
+        'process.env.PUBLIC_PATH': JSON.stringify(config.output.publicPath || '/')
+      })
+    );
+  };
